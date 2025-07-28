60072c5787fc4e504464889424850d6aef99fffa
Noeyuyg <dnlrbdus31@gmail.com>
2025-07-28 16:33:36 +0900

    - add : 데이터 추가

--- Assets/Scripts/GameDefine/CSVLoader.cs
+++ Assets/Scripts/GameDefine/CSVLoader.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 public class CSVLoader : MonoBehaviour
 {
     private const string SynergyTagPath = "CSV/synergy_tags";
+    private const string ItemListPath = "CSV/item_list";
+    private const string GrowthUpgradePath = "CSV/growth_upgrade";
 
     public static Dictionary<int, SynergyTagData> SynergyTagDict = new();
+    public static Dictionary<int, ItemCSV> ItemCSVDict = new();
+    public static Dictionary<int, GrowthUpgrade> GrowthUpgradeDict = new();
 
     private void Awake()
     {
@@ -29,6 +33,8 @@ public class CSVLoader : MonoBehaviour
     }
 
     #region Load
+
+    #region SynergyTag
     public static void LoadSynergyTag()
     {
         var csv = LoadCSV(SynergyTagPath);
@@ -72,9 +78,116 @@ public class CSVLoader : MonoBehaviour
             }
             else
             {
-                Debug.LogWarning($"중복된 키 : {key} (줄 {i + 1})");
+                Debug.LogWarning($"SynergyTag중복된 키 : {key} (줄 {i + 1})");
             }
         }
     }
+    #endregion
+
+    #region ItemList
+    public static void LoadItem()
+    {
+        var csv = LoadCSV(ItemListPath);
+
+        if (string.IsNullOrEmpty(csv))
+            return;
+
+        ItemCSVDict.Clear();
+
+        string[] lines = csv.Split('\n');
+
+        for (int i = 1; i < lines.Length; i++)
+        {
+            string line = lines[i].Trim();
+
+            if (string.IsNullOrEmpty(line)) continue;
+
+            string[] values = line.Split(",");
+
+            ItemCSV data = new ItemCSV();
+
+            data.id = int.Parse(values[0]);
+            data.name = values[1];
+            data.synergyTag1 = int.Parse(values[2]);
+            data.synergyTag2 = int.Parse(values[3]);
+            data.effectType1 = int.Parse(values[4]);
+            data.effectType2 = int.Parse(values[5]);
+            data.effectValue1 = float.Parse(values[6]);
+            data.effectValue2 = float.Parse(values[7]);
+            data.effectValueNote1 = int.Parse(values[8]);
+            data.effectValueNote2 = int.Parse(values[9]);
+            data.triggerType = int.Parse(values[10]);
+            data.triggerValue = float.Parse(values[11]);
+            data.triggerDuration = int.Parse(values[12]);
+            data.desc = values[13];
+            data.grade = int.Parse(values[14]);
+            data.weight = float.Parse(values[15]);
+            data.nameKey = values[16];
+            data.descKey = values[17];
+
+            int key = data.id;
+
+            if (!ItemCSVDict.ContainsKey(key))
+            {
+                ItemCSVDict.Add(key, data);
+            }
+            else
+            {
+                Debug.LogWarning($"LoadItem 중복된 키 : {key} (줄 {i + 1})");
+            }
+        }
+    }
+    #endregion
+
+    #region GrowthUpgrade
+    public static void LoadGrowthUpgrade()
+    {
+        var csv = LoadCSV(GrowthUpgradePath);
+
+        if (string.IsNullOrEmpty(csv))
+            return;
+
+        GrowthUpgradeDict.Clear();
+
+        string[] lines = csv.Split('\n');
+
+        for (int i = 1; i < lines.Length; i++)
+        {
+            string line = lines[i].Trim();
+
+            if (string.IsNullOrEmpty(line)) continue;
+
+            string[] values = line.Split(",");
+
+            GrowthUpgrade data = new GrowthUpgrade();
+
+            data.id = int.Parse(values[0]);
+            data.name = values[1];
+            data.desc = values[2];
+            data.iconPath = values[3];
+            data.maxLevel = int.Parse(values[4]);
+            data.baseValue = float.Parse(values[5]);
+            data.valuePerLevel = float.Parse(values[6]);
+            data.baseCost = int.Parse(values[7]);
+            data.costGrowth = float.Parse(values[8]);
+            data.valueNote = int.Parse(values[9]);
+            data.statTarget = int.Parse(values[10]);
+            data.nameKey = values[11];
+            data.descKey = values[12];
+
+            int key = data.id;
+
+            if (!GrowthUpgradeDict.ContainsKey(key))
+            {
+                GrowthUpgradeDict.Add(key, data);
+            }
+            else
+            {
+                Debug.LogWarning($"GrowthUpgrade 중복된 키 : {key} (줄 {i + 1})");
+            }
+        }
+    }
+    #endregion
+
     #endregion
 }
--- Assets/Scripts/GameDefine/GameDB.cs
+++ Assets/Scripts/GameDefine/GameDB.cs
@@ -3,17 +3,58 @@ using UnityEngine;
 [System.Serializable]
 public struct SynergyTagData
 {
-    public int tag; // (Enum) SynergyTag
+    public int tag;                 // (Enum) SynergyTag
     public string name;
     public int stage;
     public string desc;
+    public int effectType1;         // (Enum) EffectType
+    public int effectType2;         // (Enum) EffectType
+    public float effectValue1;
+    public float effectValue2;
+    public int effectValueNote1;    // (Enum) ValueNote
+    public int effectValueNote2;    // (Enum) ValueNote
+    public string colorCode;
+    public string nameKey;
+    public string descKey;
+}
+
+[System.Serializable]
+public struct ItemCSV
+{
+    public int id;
+    public string name;
+    public int synergyTag1; // (Enum) SynergyTag
+    public int synergyTag2; // (Enum) SynergyTag
     public int effectType1; // (Enum) EffectType
     public int effectType2; // (Enum) EffectType
     public float effectValue1;
     public float effectValue2;
-    public int effectValueNote1; // (Enum) ValueNote)
-    public int effectValueNote2; // (Enum) ValueNote)
-    public string colorCode;
+    public int effectValueNote1;
+    public int effectValueNote2;
+    public int triggerType; // (Enum)TiggerTarget
+    public float triggerValue;
+    public int triggerDuration;
+    public string desc;
+    public int grade;       // (Enum) TO DO
+    public float weight;
+    public string nameKey;
+    public string descKey;
+}
+
+[System.Serializable]
+public struct GrowthUpgrade
+{
+    public int id;
+    public string name;
+    public string desc;
+    public string iconPath;
+    public int maxLevel;
+    public float baseValue;
+    public float valuePerLevel;
+    public int baseCost;
+    public float costGrowth;
+    public int valueNote;   // (Enum) ValueNote
+    public int statTarget;  // (Enum) EffectType;
     public string nameKey;
     public string descKey;
 }
